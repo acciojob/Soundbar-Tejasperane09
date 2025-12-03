532b8cda17fa9761a4912530b655cfcdf1753cc6
Tejasperane09 <89592090+Tejasperane09@users.noreply.github.com>
2025-12-03 12:02:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-//your JS code here. If required.
+
+const sounds = ["applause", "boo", "gasp", "tada", "victory"];
+
+const buttonsDiv = document.getElementById("buttons");
+
+
+sounds.forEach(sound => {
+  const btn = document.createElement("button");
+  btn.className = "btn";
+  btn.innerText = sound;
+
+  const audio = new Audio(`sounds/${sound}.mp3`);
+
+  btn.addEventListener("click", () => {
+    stopAllSounds();
+    audio.currentTime = 0;
+    audio.play();
+  });
+
+  buttonsDiv.appendChild(btn);
+});
+
+
+const stopBtn = document.createElement("button");
+stopBtn.className = "stop";
+stopBtn.innerText = "Stop";
+
+stopBtn.addEventListener("click", stopAllSounds);
+
+buttonsDiv.appendChild(stopBtn);
+
+
+function stopAllSounds() {
+  document.querySelectorAll("audio").forEach(a => {
+    a.pause();
+    a.currentTime = 0;
+  });
+}
